a9d3b9bc38cbbc98f304714b933a770c65b80110
Tim Fielder <fiel1139@gmail.com>
2018-11-06 15:38:47 -0700

    initial login test spec.

---
+++ spec/requests/users_account_login_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe 'api/v1/sessions' do
+  it 'allows user to login' do
+    id = User.create(email: "whatever@example.com", password: "password").id
+    api_key = User.find_by_id(id).api_key
+    expect(User.find_by_email("whatever@example.com").id).to eq(id)
+
+    VCR.use_cassette('user_login_example') do
+      post '/api/v1/sessions', params: {
+                                "email": "whatever@example.com",
+                                "password": "password",
+                                "password_confirmation": "password"
+                               }
+
+     expect(response).to be_successful
+     expect(response.status).to eq(200)
+     expect(response.body).to have_content("api_key: #{api_key}")
+     # expect(User.find_by_email("whatever@example.com").id).to_not eq(nil)
+
+
+    end
+  end
+end
