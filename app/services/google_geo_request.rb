8feeea6f9b2a1a6363a42adac034aade03cbf646
Tim Fielder <fiel1139@gmail.com>
2018-11-06 14:59:51 -0700

    refactor dark sky request.

---
+++ app/services/google_geo_request.rb
@@ -0,0 +1,28 @@
+class GoogleGeoRequest
+  attr_reader :coordinates
+
+  def initialize(location)
+    @location = location
+    @coordinates = calculate_coordinates
+  end
+
+  private
+  def connection(url)
+    connect = Faraday.new(:url => "#{url}") do |f|
+      f.request :url_encoded
+      f.adapter Faraday.default_adapter
+    end
+  end
+
+  def get_google_geo_response
+    response = connection('https://maps.googleapis.com/maps/api/geocode/').get 'json', {:address => "#{@location}", :key => "#{ENV['google_api_key']}"}
+  end
+
+  def parse(result)
+    JSON.parse(result.body)
+  end
+
+  def calculate_coordinates
+    parse(get_google_geo_response)["results"][0]["geometry"]["location"]
+  end
+end
