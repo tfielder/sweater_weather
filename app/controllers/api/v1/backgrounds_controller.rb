38aaaf853c6542e2b55706dd3cc156d6e2f47b72
Tim Fielder <fiel1139@gmail.com>
2018-11-05 20:23:50 -0700

    changes PhotosSearch to be included in LocationSearch.

--- app/controllers/api/v1/backgrounds_controller.rb
+++
@@ -1,6 +0,0 @@
-class Api::V1::BackgroundsController < ApplicationController
-  def index
-    result = PhotoSearch.new(params[:location]).get_results
-    render json: result
-  end
-end
--- app/services/location_search.rb
+++ app/services/location_search.rb
@@ -7,7 +7,11 @@ class LocationSearch
   def get_results
     get_dark_sky_response
   end
- 
+
+  def get_photos
+    get_flikr_response
+  end
+
   private
   def connection(url)
     connect = Faraday.new(:url => "#{url}") do |f|
@@ -25,5 +29,24 @@ class LocationSearch
   def get_dark_sky_response
     response = connection("https://api.darksky.net/forecast/#{ENV['dark_sky_api_key']}/#{@coordinates["lat"]},#{@coordinates["lng"]}").get
     results = JSON.parse(response.body)
+  end
+
+  def get_flikr_response
+    response = connection("https://api.flickr.com/services/rest/").get, keys
+    results = JSON.parse(response.body)
+    binding.pry
+  end
+
+  def keys
+    {
+      :method => 'flickr.photos.search',
+      :api_key => ENV['flickr_api_key'],
+      :lat => "#{@coordinates["lat"]}",
+      :lon => "#{@coordinates["lng"]}",
+      :safe_search => "1",
+      :content_type => "1",
+      :format => "json",
+      :nojsoncallback => "1"
+    }
   end
 end
