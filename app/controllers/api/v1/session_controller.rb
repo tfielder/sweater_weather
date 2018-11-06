fd586427b3ff2eebb93e1b90f4173aa30a86e5f6
Tim Fielder <fiel1139@gmail.com>
2018-11-06 15:47:31 -0700

    stopping point with session controller.

---
+++ app/controllers/api/v1/session_controller.rb
@@ -0,0 +1,9 @@
+class Api::V1::SessionController < ApplicationController
+  # def create
+  #   if (params[:password] == params[:password_confirmation])
+  #     if User.find_by_email(params[:email]) && 
+  #   else
+  #
+  #   end
+  # end
+end
