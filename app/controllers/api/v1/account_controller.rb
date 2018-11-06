5d8a1d6c4109099339d39fff98adcc688ce5c46b
Tim Fielder <fiel1139@gmail.com>
2018-11-05 23:11:43 -0700

    migration for user.

--- app/controllers/api/v1/account_controller.rb
+++ app/controllers/api/v1/account_controller.rb
@@ -1,5 +1,17 @@
 class Api::V1::AccountController < ApplicationController
   def create
-    
+    #if !User.find_by_email(params[:email]) && (params[:password] == params[:password_confirmation])
+      #render status: 201, body: { "api_key": "#{User.find_by_email(params[:email])}"}
+
+
   end
+
+  # def lookup
+  #   #if user (password == password_confirmation) && User.find_by_email(params[:email]).password == password
+  #     #render status: 201, body: { "api_key": "#{User.find_by_email(params[:email])}"}
+  #   #else
+  #     #flash[:notice] = "Credentials could not be verified. Please try again."
+  #     #redirect to login page
+  #   #end
+  # end
 end
