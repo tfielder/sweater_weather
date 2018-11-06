e1bbd6849c803b33d2cb93c92d399c81aed84b54
Tim Fielder <fiel1139@gmail.com>
2018-11-05 21:33:45 -0700

    initial users_login_spec.rb

---
+++ spec/requests/users_login_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe 'api/v1/users' do
+  describe 'allows a user to post' do
+    it 'posts' do
+      VCR.use_cassette('user_post_example') do
+      post '/api/v1/users', {
+                              "email": "whatever@example.com",
+                              "password": "password",
+                              "password_confirmation": "password"
+                            }
+
+      expect(response).to be_successful
+      # expect(response.status).to eq(201)
+      # expect(response.body).to eq('{
+      #                               "api_key": "jgn983hy48thw9begh98h4539h4"
+      #                               }')
+      end
+    end
+  end
+end
