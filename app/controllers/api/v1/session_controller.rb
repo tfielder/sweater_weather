c216550f23bfdfbbe241ee55cf1c84d555ab6e2c
Tim Fielder <fiel1139@gmail.com>
2018-11-06 15:56:12 -0700

    session controller create action stopping point.

--- app/controllers/api/v1/session_controller.rb
+++ app/controllers/api/v1/session_controller.rb
@@ -1,7 +1,9 @@
 class Api::V1::SessionController < ApplicationController
   # def create
-  #   if (params[:password] == params[:password_confirmation])
-  #     if User.find_by_email(params[:email]) && 
+  #   if params[:password] && params[:email]
+  #     if (params[:password] == params[:password_confirmation])
+  #       user_identity_1 = User.find_by_email(params[:email])
+  #       if user_identity_1.password ==
   #   else
   #
   #   end
