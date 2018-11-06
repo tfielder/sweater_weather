683674d600995359199a6287997931c606dbb432
Tim Fielder <fiel1139@gmail.com>
2018-11-05 17:44:20 -0700

    initial start of photo search.

--- app/services/photo_search.rb
+++ app/services/photo_search.rb
@@ -1,5 +1,23 @@
-class Api::V1::PhotoSearch < ApplicationController
-  def index
+class PhotoSearch < ApplicationController
+  def initialize(location)
+    @location = location
+    @coordinates = coordinates
+  end
 
+  def get_results
+    
+  end
+
+  private
+  def connection(url)
+    connect = Faraday.new(:url => "#{url}") do |f|
+      f.request :url_encoded
+      f.adapter Faraday.default_adapter
+    end
+  end
+
+  def get_flikr_response
+    # response = connection("https://api.flickr.com/services/rest/#{ENV['dark_sky_api_key']}/#{@coordinates["lat"]},#{@coordinates["lng"]}").get
+    results = JSON.parse(response.body)
   end
 end
