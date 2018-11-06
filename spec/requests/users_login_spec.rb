f23f949f33f4093eabce566fd39eb9668a32f2ae
Tim Fielder <fiel1139@gmail.com>
2018-11-05 21:37:39 -0700

    revise params in users_login_spec.

--- spec/requests/users_login_spec.rb
+++ spec/requests/users_login_spec.rb
@@ -4,7 +4,7 @@ describe 'api/v1/users' do
   describe 'allows a user to post' do
     it 'posts' do
       VCR.use_cassette('user_post_example') do
-      post '/api/v1/users', {
+      post '/api/v1/users', params: {
                               "email": "whatever@example.com",
                               "password": "password",
                               "password_confirmation": "password"
