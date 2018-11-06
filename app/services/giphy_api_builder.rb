2f2bee76b627a4ee57f93771935ab0771872b534
Tim Fielder <fiel1139@gmail.com>
2018-11-06 11:15:52 -0700

    moves giphy request out of giphy api builder.

--- app/services/giphy_api_builder.rb
+++ app/services/giphy_api_builder.rb
@@ -6,41 +6,17 @@ class GiphyApiBuilder
   def return_response
     {
       data: {
-       images: array_builder
-     },
-        copyright: "2018"
+              images: array_builder
+            },
+      copyright: "2018"
     }
   end
 
   private
-  def connection(url)
-    connect = Faraday.new(:url => "#{url}") do |f|
-      f.request :url_encoded
-      f.adapter Faraday.default_adapter
-    end
-  end
 
-  def get_giphy_response(term)
-    response = connection("https://api.giphy.com/v1/gifs/search").get '', giphy_keys(term)
-  end
-
-  def parse(result)
-    JSON.parse(result.body)
-  end
-
-  def giphy_keys(term)
-    {
-      :api_key  => ENV['giphy_api_key'],
-      :q        => "#{term}"
-    }
-  end
 
   def weekly_outlook
     @outlook = @dark_sky["daily"]["data"]
-  end
-
-  def get_giphy(term)
-    result = parse(get_giphy_response(term))
   end
 
   def array_builder
@@ -48,8 +24,11 @@ class GiphyApiBuilder
       new_hash = {}
       new_hash[:time] = hash["time"]
       new_hash[:summary] = hash["summary"]
-      giphy_result = get_giphy(hash["summary"].gsub(/ /, '+'))
-      new_hash[:url] = giphy_result["data"][0]["url"]
+      # giphy_result = get_giphy(hash["summary"].gsub(/ /, '+'))
+      # giphy_result = GiphyRequest.new(hash["summary"]).get_giphy_response
+      # giphy_result = GiphyRequest.new(hash["summary"]).get_url
+      # new_hash[:url] = giphy_result["data"][0]["url"]
+      new_hash[:url] = GiphyRequest.new(hash["summary"]).get_url
       new_hash
     end
   end
