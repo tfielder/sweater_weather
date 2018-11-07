007ed08b69b3a95cf6ab23deaa28154088e684f8
Tim Fielder <fiel1139@gmail.com>
2018-11-06 20:43:41 -0700

    refactor user controller rendering.

--- spec/requests/users_account_creation_spec.rb
+++ spec/requests/users_account_creation_spec.rb
@@ -4,6 +4,7 @@ describe 'api/v1/users' do
   describe 'allows a user to post' do
     it 'posts to create a new user' do
       expect(User.find_by_email("whatever@example.com")).to eq(nil)
+      expect(User.all.count).to eq(0)
 
       VCR.use_cassette('user_post_example') do
       post '/api/v1/users', params: {
@@ -15,6 +16,10 @@ describe 'api/v1/users' do
       expect(response).to be_successful
       expect(response.status).to eq(201)
       expect(User.find_by_email("whatever@example.com").id).to_not eq(nil)
+      expect(User.all.count).to eq(1)
+
+      api_key = User.first.api_key
+      expect(JSON.parse(response.body)).to eq({"api_key" => "#{api_key}"})
       end
     end
   end
